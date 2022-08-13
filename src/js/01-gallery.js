c3ff940440f97b9decbc0d2f97a24db2d8a61255
olga-plotkina <olga.semenyuta@gmail.com>
2022-08-13 19:59:56 +0100

    updated gallery-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
 console.log(galleryItems);
+
+const galleryDiv = document.querySelector('.gallery');
+
+const renderGallery = (galleryItems) => {
+    return galleryItems.map(({preview, original, description}) => 
+    `<a class="gallery__item" href="${original}"> 
+             <img class="gallery__image" 
+                src="${preview}"
+                alt="${description}"> 
+    </a>
+    `).join('');
+};
+
+galleryDiv.innerHTML = renderGallery(galleryItems);
+
+
+let lightbox = new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250});
+
+
