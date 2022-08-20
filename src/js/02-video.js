1df94721156997f505a62035c9093e8a9c6dd44b
olga-plotkina <olga.semenyuta@gmail.com>
2022-08-20 11:48:44 +0100

    testing prettier

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,19 +4,20 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-const onPlay = ({seconds}) => {
-    localStorage.setItem("videoplayer-current-time", JSON.stringify(seconds));
-}
+const onPlay = ({ seconds }) => {
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(seconds));
+};
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
-
-player.setCurrentTime(JSON.parse(localStorage.getItem("videoplayer-current-time"))).then(function(seconds) {
-}).catch(function(error) {
+player
+  .setCurrentTime(JSON.parse(localStorage.getItem('videoplayer-current-time')))
+  .then(function (seconds) {})
+  .catch(function (error) {
     switch (error.name) {
-        case 'RangeError':
-            break;
-        default:
-            break;
+      case 'RangeError':
+        break;
+      default:
+        break;
     }
-});
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,34 +1,33 @@
-import throttle from "lodash.throttle";
+import throttle from 'lodash.throttle';
 
 const formRef = document.querySelector('.feedback-form');
 
 const savedMessage = JSON.parse(localStorage.getItem('feedback-form-state'));
 
 if (savedMessage) {
-    formRef.elements.email.value = savedMessage.email;
-    formRef.elements.message.value = savedMessage.message;
+  formRef.elements.email.value = savedMessage.email;
+  formRef.elements.message.value = savedMessage.message;
 }
 
 const formData = {};
 
-const onFormInput = (event) => {
-    formData[event.target.name] = event.target.value;  
-    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+const onFormInput = event => {
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 };
 
 formRef.addEventListener('input', throttle(onFormInput, 500));
 
-const onFormSubmit = (event) => {
-    event.preventDefault();
-    const {email, message} = event.currentTarget.elements;
-    
-    formData.email = email.value;
-    formData.message = message.value;
+const onFormSubmit = event => {
+  event.preventDefault();
+  const { email, message } = event.currentTarget.elements;
 
-    event.currentTarget.reset();
-    localStorage.removeItem('feedback-form-state');
-    console.log(formData);
+  formData.email = email.value;
+  formData.message = message.value;
+
+  event.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+  console.log(formData);
 };
 
 formRef.addEventListener('submit', onFormSubmit);
-
