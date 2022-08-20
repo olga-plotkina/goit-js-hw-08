af8162499090545740e549c430976ca545443594
olga-plotkina <olga.semenyuta@gmail.com>
2022-08-20 15:08:28 +0100

    added const STORAGE_KEY

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,15 +1,15 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'videoplayer-current-time';
+
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
 const onPlay = ({ seconds }) => {
-  localStorage.setItem('videoplayer-current-time', JSON.stringify(seconds));
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(seconds));
 };
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
-player.setCurrentTime(
-  JSON.parse(localStorage.getItem('videoplayer-current-time'))
-);
+player.setCurrentTime(JSON.parse(localStorage.getItem(STORAGE_KEY)));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,7 +2,8 @@ import throttle from 'lodash.throttle';
 
 const formRef = document.querySelector('.feedback-form');
 
-const savedMessage = JSON.parse(localStorage.getItem('feedback-form-state'));
+const STORAGE_KEY = 'feedback-form-state';
+const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
 const formData = savedMessage ?? {};
 
 if (savedMessage) {
@@ -14,7 +15,7 @@ if (savedMessage) {
 
 const onFormInput = event => {
   formData[event.target.name] = event.target.value;
-  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 };
 
 formRef.addEventListener('input', throttle(onFormInput, 500));
@@ -34,7 +35,7 @@ const onFormSubmit = event => {
   }
 
   event.currentTarget.reset();
-  localStorage.removeItem('feedback-form-state');
+  localStorage.removeItem(STORAGE_KEY);
   console.log(formData);
 };
 
