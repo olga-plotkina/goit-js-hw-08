901a756874746f55f80346935df55a650cb54b42
olga-plotkina <olga.semenyuta@gmail.com>
2022-08-20 13:10:58 +0100

    changed formData

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,13 +3,13 @@ import throttle from 'lodash.throttle';
 const formRef = document.querySelector('.feedback-form');
 
 const savedMessage = JSON.parse(localStorage.getItem('feedback-form-state'));
+const formData = {};
 
 if (savedMessage) {
   formRef.elements.email.value = savedMessage.email;
   formRef.elements.message.value = savedMessage.message;
+  formData = savedMessage;
 }
-
-const formData = {};
 
 const onFormInput = event => {
   formData[event.target.name] = event.target.value;
